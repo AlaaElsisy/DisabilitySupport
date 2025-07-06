6b86eca52c31697dd79de381c51a179011f19f8f
Ahmed Abu Elmagd <abuelmagdahmed6@gmail.com>
2025-07-06 12:17:15 +0300

    UseStaticFiles

--- DisabilitySupport.DAL/Models/ServiceCategory.cs
+++ DisabilitySupport.DAL/Models/ServiceCategory.cs
@@ -11,6 +11,7 @@ namespace DisabilitySupport.DAL.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string? Description { get; set; }
+        public string? Image { get; set; }
 
         // Navigation properties
         public virtual List<HelperService>? HelperServices { get; set; } = new List<HelperService>();
